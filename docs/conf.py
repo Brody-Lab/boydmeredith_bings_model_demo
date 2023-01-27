641e95c24ddb66a257e33e84a2e5dc8f322a8a73
Tyler Boyd-Meredith <jboydmeredith@gmail.com>
2023-01-26 19:13:37 -0500

    update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,6 @@ author = 'Tyler Boyd-Meredith'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'jupyter_sphinx',
     'sphinx.ext.napoleon',
     'sphinx.ext.autodoc'
 ]
