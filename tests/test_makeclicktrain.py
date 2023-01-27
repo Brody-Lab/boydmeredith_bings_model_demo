10d7026fb35e25125fb8a66e4e63971494754ac1
Tyler Boyd-Meredith <jboydmeredith@gmail.com>
2023-01-27 16:07:33 -0500

    add unit test to make sure click rate is correct

---
+++ tests/test_makeclicktrain.py
@@ -0,0 +1,13 @@
+
+import bings_model_demo.model as bm 
+
+
+def test_totalrate():
+    total_rate_in = 40
+    dur = 10000
+    bups = bm.make_clicktrain(total_rate=total_rate_in, duration=dur)
+    leftbups, rightbups = bups['left'], bups['right']
+    nleft, nright = len(leftbups), len(rightbups)
+    total_rate_out = (nleft+nright)/dur
+    print(total_rate_in, total_rate_out)
+    assert(abs(total_rate_in-total_rate_out)< .1)
