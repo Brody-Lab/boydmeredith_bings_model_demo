ee1900641c906fc186ed156616898e69bb1164e8
tyler <jboydmeredith@gmail.com>
2023-01-30 15:19:12 -0500

    make function to draw gui; add model notes

--- src/bings_model_demo/gui.py
+++ src/bings_model_demo/gui.py
@@ -1,6 +1,6 @@
 from ipywidgets import widgets, AppLayout, interact, interactive, fixed
 from IPython.display import display, Math, Latex
-import numpy as np 
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -11,43 +11,43 @@ sns.set_context('notebook')
 
 def clicks_eventhandler(change):
     global bups
-    bups = bm.make_clicktrain(total_rate=rate_slider.value, gamma=gamma_slider.value, 
+    bups = bm.make_clicktrain(total_rate=rate_slider.value, gamma=gamma_slider.value,
                                duration=dur_slider.value, rng=seed_slider.value)
     adaptation_eventhandler(change)
-    
+
 def adaptation_eventhandler(change):
-    bm.make_adapted_clicks(bups, phi=phi_slider.value, tau_phi=tau_phi_slider.value)  
+    bm.make_adapted_clicks(bups, phi=phi_slider.value, tau_phi=tau_phi_slider.value)
     integration_eventhandler(change)
-    
+
 def integration_eventhandler(change):
     global a, params
     a, params = bm.integrate_adapted_clicks(bups, lam=lambda_slider.value, s2s=s2s_slider.value,
                              s2a=s2a_slider.value, s2i=s2i_slider.value, bias=bias_slider.value,
                              B=B_slider.value, nagents=nagent_slider.value, rng=seed_slider.value)
     choice_eventhandler(change)
-    
+
 def choice_eventhandler(change):
-    plot_out.clear_output(wait=True)    
+    plot_out.clear_output(wait=True)
     with plot_out:
         dp.plot_process(bups, a, params)
-        
+
 plot_out = widgets.Output()
 
 bups_text = widgets.Output()
 with bups_text:
     print("Click train parameters\n")
-    
+
 adaptation_text = widgets.Output()
 with adaptation_text:
     print("\nAdaptation parameters\n")
     #display(Math(r'$dC = \frac{1-C}{\tau_{\phi}}dt + (\phi - 1) C (\delta_{t,t_R} + \delta_{t,t_L})$'))
-    
+
 integration_text = widgets.Output()
 with integration_text:
     print("\n\nAccumulation parameters \n")
     #display(Math(r'$da = (\eta C \delta_R - \eta C \delta_L)dt + \lambda a dt + \sigma_adW $'))
     #display(Math('$a_0 = \mathcal{N}(0, \sigma_i^2)$'))
-    
+
 choice_text = widgets.Output()
 with choice_text:
     print("\nChoice parameters\n")
@@ -57,7 +57,7 @@ seed_slider = widgets.IntSlider(value=1, min=0, max=10, description="Seed")
 rate_slider = widgets.IntSlider(value=40,min=5,max=50,step=5,description=r'$r_L+r_R$ (Hz)')
 gamma_slider = widgets.FloatSlider(value=1.5,min=-5,max=5,step=.25,description=r"$\gamma = \log \frac{r_1}{r_2}$")
 dur_slider = widgets.FloatSlider(value=1,min=.25,max=5,step=.25,description="T (s)")
-        
+
 seed_slider.observe(clicks_eventhandler, names='value')
 rate_slider.observe(clicks_eventhandler, names='value')
 gamma_slider.observe(clicks_eventhandler, names='value')
@@ -93,11 +93,13 @@ lapse_slider.observe(clicks_eventhandler, names='value')
 
 bup_inputs = widgets.VBox([bups_text, seed_slider, rate_slider, gamma_slider, dur_slider])
 adaptation_inputs = widgets.VBox([adaptation_text, phi_slider, tau_phi_slider])
-integration_inputs = widgets.VBox([integration_text, lambda_slider, s2s_slider, s2a_slider, 
+integration_inputs = widgets.VBox([integration_text, lambda_slider, s2s_slider, s2a_slider,
                                    s2i_slider, B_slider, nagent_slider])
 choice_inputs = widgets.VBox([choice_text, bias_slider, lapse_slider])
 inputs = widgets.VBox([bup_inputs, adaptation_inputs, integration_inputs, choice_inputs])
 
 interface = widgets.HBox([inputs, plot_out])
-display(interface)
-clicks_eventhandler([])
+
+def draw_gui():
+    display(interface)
+    clicks_eventhandler([])
